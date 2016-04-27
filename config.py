c159df5ae404f6be79e77a601d9147510761ecb1
Matt Gaspar <matt.gaspar@digital.landregistry.gov.uk>
2016-04-27 09:44:47 +0100

    Remove special characters

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ import os
 class Config(object):
     DEBUG = os.getenv('DEBUG', False)
     SECRET_KEY = os.getenv('SECRET_KEY','123456reallysecretkey')
-    EXCLUDE_CHARS = ["$","'","`"]
+    EXCLUDE_CHARS = os.getenv('EXCLUDE_CHARS','')
